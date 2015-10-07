22e817e16d7e84a458159e3b55eba717959898ed
Paul Allen <pauldallen@gmail.com>
2015-10-08 00:43:23 +0200

    ./ added to the command

--- boot-scripts/install_etcd.sh
+++ boot-scripts/install_etcd.sh
@@ -38,7 +38,7 @@ if [ "true" = "$IS_LEADER" ]; then
 	-initial-cluster "$name=http://${MY_IPADDRESS}:2380" 2>&1 >> "$etcd_log_file" &
 else
     ## we are not the leaders, lets generate a join string
-    add_member_output="$(etcdctl --endpoint "http://${cluster_name}.${dns_zone}:2379" member add "$name" "http://${MY_IPADDRESS}:2380")"
+    add_member_output="$(./etcdctl --endpoint "http://${cluster_name}.${dns_zone}:2379" member add "$name" "http://${MY_IPADDRESS}:2380")"
     etcd_name="$(echo $add_member_output | grep ETCD_NAME | awk -F'\"' '{print $2}')"
     etcd_initial_cluster="$(echo $add_member_output | grep ETCD_INITIAL_CLUSTER | awk -F'\"' '{print $2}')"
     etcd_initial_cluster_state="$(echo $add_member_output | grep ETCD_INITIAL_CLUSTER_STATE | awk -F'\"' '{print $2}')"
