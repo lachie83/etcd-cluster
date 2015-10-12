ab7f0901f4ba61b0dbd318d08a111acc3ba26911
Paul Allen <pauldallen@gmail.com>
2015-10-12 18:12:10 +0200

    exit if the request is bad

--- boot-scripts/remove_stale_members.sh
+++ boot-scripts/remove_stale_members.sh
@@ -2,12 +2,23 @@
 
 source /etc/profile.d/cluster
 
+addresses="$(python ../extends/boot-scripts/lib/group_addresses.py)"
+
+## if we aren't in the address list it was a bad request
+if ! echo "$addresses" | grep -q "$MY_IPADDRESS"; then
+    exit 0
+fi
 (
     cd /opt/etcd
-    member_list_output="$(./etcdctl cluster-health | grep -e unhealthy -e unreachable)"
-    echo "$member_list_output" | while read word id is status ip therest; do
-	if [ -n "$id" ]; then
-            ./etcdctl member remove "$id"
-	fi
+    member_list_output="$(./etcdctl member list)"
+    echo "$member_list_output"
+    echo
+    echo "$member_list_output" | while read id name peer client; do
+        member_ip=$(echo "$peer" | awk -F':' '{print $2}' | perl -pe 's{/}{}g')
+        if ! echo "$addresses" | grep -q "$member_ip"; then
+            echo "removing stale member from cluster: $member_ip"
+                member_id="$(echo "$member_list_output" | grep "$member_ip" | awk -F':' '{print $1}')"
+                    ./etcdctl member remove "$member_id"
+        fi
     done
 )
