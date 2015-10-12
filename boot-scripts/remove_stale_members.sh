7a3b1cd4c25a740dccb44f3ea985a582b4c3f636
Paul Allen <pauldallen@gmail.com>
2015-10-12 15:41:51 +0200

    check my ip too

--- boot-scripts/remove_stale_members.sh
+++ boot-scripts/remove_stale_members.sh
@@ -7,7 +7,7 @@ source /etc/profile.d/cluster
     member_list_output="$(./etcdctl member list)"
     echo "$member_list_output" | while read id name peer client; do
         member_ip=$(echo "$peer" | awk -F':' '{print $2}' | perl -pe 's{/}{}g')
-        if ! echo "$CLUSTER_ADDRESSES" | grep -q "$member_ip"; then
+        if ! echo "$CLUSTER_ADDRESSES,$MY_IPADDRESS" | grep -q "$member_ip"; then
             echo "removing stale member from cluster: $member_ip"
             ## remove the member
         fi
