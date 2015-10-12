6716c0e006fc8040f4637d1f721d5555818d37a5
Paul Allen <pauldallen@gmail.com>
2015-10-12 15:30:40 +0200

    fixing up the remove stale script

--- boot-scripts/remove_stale_members.sh
+++ boot-scripts/remove_stale_members.sh
@@ -2,10 +2,14 @@
 
 source /etc/profile.d/cluster
 
-member_list_output="$()"
-for member_ip in "$member_list_output"; do
-    if ! echo "$CLUSTER_ADDRESSES" | grep -q "$member_ip"; then
-	echo "removing stale member from cluster: $member_ip"
-	## remove the member
-    fi
-done
+(
+    cd /opt/etcd
+    member_list_output="$(./etcdctl member list)"
+    echo "$member_list_output" | while read id name peer client; do
+        member_ip=$(echo "$peer" | awk -F':' '{print $2}' | perl -pe 's{/}{}g')
+        if ! echo "$CLUSTER_ADDRESSES" | grep -q "$member_ip"; then
+            echo "removing stale member from cluster: $member_ip"
+            ## remove the member
+        fi
+    done
+)
