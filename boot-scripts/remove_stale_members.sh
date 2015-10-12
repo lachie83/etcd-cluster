9b5541a32d9fabaac165276f373046ecc7c3ca95
Paul Allen <pauldallen@gmail.com>
2015-10-12 16:51:29 +0200

    don't apperate on nothing

--- boot-scripts/remove_stale_members.sh
+++ boot-scripts/remove_stale_members.sh
@@ -6,6 +6,8 @@ source /etc/profile.d/cluster
     cd /opt/etcd
     member_list_output="$(./etcdctl cluster-health | grep -e unhealthy -e unreachable)"
     echo "$member_list_output" | while read word id is status ip therest; do
-        ./etcdctl member remove "$id"
+	if [ -n "$id" ]; then
+            ./etcdctl member remove "$id"
+	fi
     done
 )
