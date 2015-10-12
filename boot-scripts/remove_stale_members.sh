4396f4825deebe0f37d0910cb406c084f45c56e3
Paul Allen <pauldallen@gmail.com>
2015-10-12 15:53:31 +0200

    actually do the member removal

--- boot-scripts/remove_stale_members.sh
+++ boot-scripts/remove_stale_members.sh
@@ -9,7 +9,8 @@ source /etc/profile.d/cluster
         member_ip=$(echo "$peer" | awk -F':' '{print $2}' | perl -pe 's{/}{}g')
         if ! echo "$CLUSTER_ADDRESSES,$MY_IPADDRESS" | grep -q "$member_ip"; then
             echo "removing stale member from cluster: $member_ip"
-            ## remove the member
+	    member_id="$(echo "$member_list_output" | grep "$member_ip" | awk -F':' '{print $1}')"
+	    ./etcdctl member remove "$member_id"
         fi
     done
 )
