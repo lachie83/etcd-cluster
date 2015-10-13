8f88ab4be2c1f3935381f72e254066e1854f1a5b
Paul Allen <pauldallen@gmail.com>
2015-10-13 11:31:54 +0200

    use cluster-health to check for health and existence

--- boot-scripts/remove_stale_members.sh
+++ boot-scripts/remove_stale_members.sh
@@ -6,31 +6,37 @@ script="/usr/bin/remove_unhealthy.sh"
 
 cat <<"EOF" > "$script"
 #!/bin/bash
+
 source /etc/profile.d/cluster
 
 addresses="$(python /usr/bin/group_addresses.py)"
 
 ## if we aren't in the address list it was a bad request
+
 if ! echo "$addresses" | grep -q "$MY_IPADDRESS"; then
     exit 0
 fi
 (
     cd /opt/etcd
-    member_list_output="$(./etcdctl member list)"
-    echo "$member_list_output"
+    cluster_health_output="$(./etcdctl cluster-health | grep "member")"
+    echo "$cluster_health_output"
     echo
-    echo "$member_list_output" | while read id name peer client; do
-        member_ip=$(echo "$peer" | awk -F':' '{print $2}' | perl -pe 's{/}{}g')
-        if ! echo "$addresses" | grep -q "$member_ip"; then
+    echo "$cluster_health_output" | while read member member_id therest; do
+        health=$(echo "$therest" | awk -F':' '{print $2}' | perl -pe 's{/}{}g')
+        member_ip=$(echo "$therest" | awk -F':' '{print $3}' | perl -pe 's{/}{}g')
+        ### first, lets remove any unhealthy members
+
+        echo "checking id:$member_id on ip:$member_ip which is $health"
+        if (echo "$health" | grep -q unhealthy) || (! echo "$addresses" | grep -q "$member_ip"); then
             echo "removing stale member from cluster: $member_ip"
-                member_id="$(echo "$member_list_output" | grep "$member_ip" | awk -F':' '{print $1}')"
-                    ./etcdctl member remove "$member_id"
+            ./etcdctl member remove "$member_id"
         fi
     done
 )
+
 
 EOF
 
 chmod +x "$script"
 
-echo "* * * * * $script > /var/log/remove_unhealthy.log" | crontab
+echo "* * * * * $script >> /var/log/remove_unhealthy.log" | crontab
